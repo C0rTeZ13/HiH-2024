6fbdb219fd9843bff7a95241a48a0202c053709c
Дмитрий Панфилов Геннадьевич ПИН-11 <panf1loff.dmitry@yandex.ru>
2024-12-01 00:47:49 +0300

    Сделал отправку изображения и данных на сервер

--- Frontend/Qt/src/MainWindow.cpp
+++ Frontend/Qt/src/MainWindow.cpp
@@ -11,13 +11,14 @@
 #include <QDebug>
 
 #include <Authorization.h>
+#include <QHttpPart>
 
 CMainWindow::CMainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::CMainWindow)
 {
     ui->setupUi(this);
-
+    _url = QString("http://94.103.84.36:5086/");
     //setWindowFlags(Qt::FramelessWindowHint| Qt::WindowSystemMenuHint);
     // to fix taskbar minimize feature
     //setWindowFlags(windowFlags() | Qt::WindowMinimizeButtonHint);
@@ -31,55 +32,101 @@ CMainWindow::~CMainWindow()
     delete ui;
 }
 
+void CMainWindow::sendImageToServer(const QImage &image)
+{
+    // Сохранение изображения в байтовый массив
+    QByteArray byteArray;
+    QBuffer buffer(&byteArray);
+    buffer.open(QIODevice::WriteOnly);
+    image.save(&buffer, "JPG"); // Сохраняем изображение в формате JPG
+    buffer.close();
+
+    // Создание multipart-запроса
+    QHttpMultiPart *multiPart = new QHttpMultiPart(QHttpMultiPart::FormDataType);
+
+    // Создание части запроса для файла
+    QHttpPart filePart;
+    QString filename = "filename=" + QFileInfo(ui->params->getImage()).fileName();
+    filePart.setHeader(QNetworkRequest::ContentDispositionHeader,
+                       QVariant("form-data; name=\"file\"; " + filename));
+    filePart.setHeader(QNetworkRequest::ContentTypeHeader, QVariant("image/jpeg"));
+    filePart.setBody(byteArray);
+    multiPart->append(filePart);
+
+    QUrl url(_url + "Image/UploadImage");
+    QNetworkRequest request(url);
+    QString authHeader = "Bearer " + _adminToken;
+    request.setRawHeader("Authorization", authHeader.toUtf8());
+
+    QNetworkAccessManager *imageManager = new QNetworkAccessManager(this);
+    QNetworkReply *imageReply = imageManager->post(request, multiPart);
+
+
+    multiPart->setParent(imageReply);
+
+    connect(imageReply, &QNetworkReply::finished, this, &CMainWindow::onImageUpload);
+}
+
+void CMainWindow::sendDataToServer(QJsonObject json)
+{
+    QNetworkAccessManager *manager = new QNetworkAccessManager();
+    QUrl lUrl(_url + "DrawEstimates/CreateEstimates");
+    QNetworkRequest request(lUrl);
+    QString authHeader = "Bearer " + _adminToken;
+    request.setRawHeader(QByteArray("Authorization"), QByteArray(authHeader.toUtf8()));
+    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
+    QJsonDocument doc(json);
+    QByteArray jsonData = doc.toJson();
+    auto dataReply = manager->post(request, jsonData);
+
+    connect(dataReply, &QNetworkReply::finished, this, &CMainWindow::onDataUpload);
+}
+
+void CMainWindow::onImageUpload()
+{
+    QNetworkReply *reply = qobject_cast<QNetworkReply *>(sender());
+    if (reply->error() == QNetworkReply::NoError) {
+        StructParams params = *ui->params->getParams();
+        int idImage = QJsonDocument::fromJson(reply->readAll())["id"].toInt();
+        int standartSizeMillimeters = params.m_standardSizeMillimeters;
+        QString standardDetail = params.standardDetail;
+        int torchWidth = params.m_torchWidthMillimeters;
+        int torchTakeOff = params.m_paintMillilitersPerSquareMeter;
+        int costPerLiter = params.m_coastPerLiter;
+        int paintMmPerSquareMeter = params.m_paintMillilitersPerSquareMeter;
+
+        QJsonObject json;
+        json["originImageId"] = idImage;
+        json["standardSizeMillimeters"] = standartSizeMillimeters;
+        json["standardDetail"] = standardDetail;
+        json["torchWidthMillimeters"] = torchWidth;
+        json["torchTakeoffMillimeters"] = torchTakeOff;
+        json["coastPerLiter"] = costPerLiter;
+        json["paintMillilitersPerSquareMeter"] = paintMmPerSquareMeter;
+        sendDataToServer(json);
+    } else {
+        qDebug() << "Error in uploading image to the server:" << reply->errorString();
+    }
+
+    reply->deleteLater();
+}
+
+void CMainWindow::onDataUpload()
+{
+    QNetworkReply *reply = qobject_cast<QNetworkReply *>(sender());
+    if(reply->error() == QNetworkReply::NoError) {
+        // ОБРАБОТАN ТОГО, КОГДА ВСЁ ЗАЕБИСЬ
+    } else {
+        qDebug() << "Error in data uploading" << reply->errorString();
+    }
+    reply->deleteLater();
+}
+
 void CMainWindow::on_btn_calc_clicked()
 {
     QString imageName = ui->params->getImage();
     QImage image(imageName);
-
-    StructParams params = *ui->params->getParams();
-
-    //auto size = ui->params->getStandartSize();
-    int standartSizeMillimeters = params.m_standardSizeMillimeters;
-    QString standardDetail = params.standardDetail;
-    int torchWidth = params.m_torchWidthMillimeters;
-    int torchTakeOff = params.m_paintMillilitersPerSquareMeter;
-    int costPerLiter = params.m_coastPerLiter;
-    int paintMmPerSquareMeter = params.m_paintMillilitersPerSquareMeter;
-
-    QNetworkAccessManager *manager = new QNetworkAccessManager ();
-    QUrl url("http://94.103.84.36:5086/");
-    QNetworkRequest request(url);
-
-    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
-
-    //QJsonArray sizeArray = {size->m_bonnet, size->m_frontDoor, size->m_trunkLid};
-    QByteArray byteArray;
-    QBuffer buffer(&byteArray);
-    image.save(&buffer, "PNG");
-    QByteArray base64Data = byteArray.toBase64();
-
-    QJsonObject json;
-
-    json["image"] = QString::fromLatin1(base64Data);
-    //json["size"] = sizeArray;
-    json["standardSizeMillimeters"] = standartSizeMillimeters;
-    json["standardDetail"] = standardDetail;
-    json["torchWidthMillimeters"] = torchWidth;
-    json["torchTakeoffMillimeters"] = torchTakeOff;
-    json["coastPerLiter"] = costPerLiter;
-    json["paintMillilitersPerSquareMeter"] = paintMmPerSquareMeter;
-
-    QJsonDocument doc(json);
-    QByteArray data = doc.toJson();
-    QNetworkReply *reply = manager->post(request, data);
-
-    QObject::connect(reply, &QNetworkReply::finished, [=]() {
-        if (reply->error() == QNetworkReply::NoError) {
-            qDebug() << "Ответ сервера: " << reply->readAll();
-        } else {
-            qDebug() << "Ошибка: " << reply->errorString();
-        }
-    });
+    sendImageToServer(image);
 }
 
 void CMainWindow::setScene(QString filepath)
@@ -92,4 +139,9 @@ void CMainWindow::setScene(QString filepath)
 void CMainWindow::setAuthorize(QString login)
 {
     ui->edit_login->setText(login);
+}
+
+void CMainWindow::setToken(QString token)
+{
+    _token = token;
 }
